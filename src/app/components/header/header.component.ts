0e842e9c16c01d33bd1fa21bde27056afc3e2988
Michał Płużek <michalpluzek@gmail.com>
2022-03-16 14:28:37 +0100

    Add header layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faShippingFast,
+  faSearch,
+  faCartPlus,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeaderComponent implements OnInit {
+  faShippingFast = faShippingFast;
+  faSearch = faSearch;
+  faCartPlus = faCartPlus;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
