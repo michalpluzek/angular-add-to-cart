20cf36c66f3b81710ca03c9606c9dcc614a13b58
Michał Płużek <michalpluzek@gmail.com>
2022-03-17 11:15:32 +0100

    Add cart page layout and service

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styles: [
-  ]
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  faTrash = faTrash;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { ApiService } from 'src/app/services/api.service';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductInterface } from 'src/app/types/product.interface';
 
 @Component({
@@ -12,9 +14,16 @@ import { ProductInterface } from 'src/app/types/product.interface';
 export class ProductsComponent implements OnInit {
   public productList$!: Observable<ProductInterface[]>;
 
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     this.productList$ = this.apiService.getProducts();
+  }
+
+  addToCart(product: ProductInterface): void {
+    this.cartService.addToCart(product);
   }
 }
